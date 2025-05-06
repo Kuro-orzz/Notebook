13231a4418703c5540f28bc218a4d3a73856c333
Kuro-orzz <23020674@vnu.edu.vn>
2025-05-06 20:01:39 +0700

    update

---
+++ DataStructure/SegTree/SegTreeBeats/test3.cpp
@@ -0,0 +1,32 @@
+// https://judge.yosupo.jp/problem/range_chmin_chmax_add_range_sum
+
+#include "../../../template.h"
+#include "SegTreeBeats3.h"
+
+void solve() {
+	int n, q; cin >> n >> q;
+	SegTreeBeats st(n);
+	for (int i = 1; i <= n; i++) {
+		int x; cin >> x;
+		st.build(1, 1, n, i, x);
+	}
+	// for (int i = 1; i <= n; i++) cout << st.getSum(1, 1, n, i, i) << ' ';
+	// 	cout << endl;
+	while (q--) {
+		int type, l, r; cin >> type >> l >> r;
+		if (type == 0) {
+			int b; cin >> b;
+			st.updateChmin(1, 1, n, l+1, r, b);
+		} else if (type == 1) {
+			int b; cin >> b;
+			st.updateChmax(1, 1, n, l+1, r, b);
+		} else if (type == 2) {
+			int b; cin >> b;
+			st.updateRange(1, 1, n, l+1, r, b);
+		} else if (type == 3) {
+			cout << st.getSum(1, 1, n, l+1, r) << '\n';
+		}		
+		// for (int i = 1; i <= n; i++) cout << st.getSum(1, 1, n, i, i) << ' ';
+		// cout << endl;
+	}
+}
