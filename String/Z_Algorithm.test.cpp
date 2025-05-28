ec43cf6f8a38ecb1ece959d56047802de13b9861
Kuro-orzz <23020674@vnu.edu.vn>
2025-05-29 01:30:55 +0700

    add z function

---
+++ String/Z_Algorithm.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/zalgorithm"
+
+#include "../template.h"
+#include "Z_Function.h"
+
+void solve() {
+	string s; cin >> s;
+	vector<int> ans = Z_function(s);
+	for (int x : ans) cout << x << " ";
+}
