7507709526f5b3226390215df0e99a9916fe2ba8
Kuro-orzz <23020674@vnu.edu.vn>
2025-05-30 02:37:12 +0700

    add kmp

---
+++ String/Kmp.h
@@ -0,0 +1,34 @@
+#include "../template.h"
+
+vector<int> lps(const string &s) {
+	int n = s.size();
+	vector<int> pi(n);
+	for (int i = 1; i < n; i++) {
+		int j = pi[i-1];
+		while (j > 0 && s[i] != s[j]) {
+			j = pi[j - 1];
+		}
+		if (s[i] == s[j]) j++;
+		pi[i] = j;
+	}
+	return pi;
+}
+
+vector<int> __KMP(const string &s, const string &t) {
+	int n = s.size(), m = t.size();
+	if (n == 0) return {};
+	vector<int> pi = lps(s);
+	int j = 0;
+	vector<int> ans(m);
+	for (int i = 0; i < m; i++) {
+		while (j > 0 && t[i] != s[j]) {
+			j = pi[j - 1];
+		}
+		if (t[i] == s[j]) j++;
+		ans[i] = j;
+		if (j == n) {
+			j = pi[j - 1];
+		}
+	}
+	return ans;
+}
